52cf47eeeb735153d4ba7956a3603b376a83cc03
ced <momo9795579@gmail.com>
2023-01-08 20:35:58 +0900

    fire

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,28 @@ mod status;
 use dotenvy::dotenv;
 use rust_socketio::{ClientBuilder, Payload, RawClient};
 use serde_json::json;
-use std::env;
+use std::{env, any::Any};
 use std::time::Duration;
 use sysinfo::{CpuExt, System, SystemExt};
 
-use crate::status::{CpuData, CpuCoreUsage, StatusData};
+use crate::status::{CpuCoreUsage, CpuData, StatusData};
 
 fn main() {
     dotenv().expect(".env file not found");
 
-    /*for (key, value) in env::vars() {
-        println!("{key}: {value}");
-    }*/
+    let PASS = match env::var("PASS") {
+        Ok(val) => val,
+        Err(_) => "".to_string(),
+    };
+
+    let PCSC_URI = match env::var("PCSC_URI") {
+        Ok(val) => val,
+        Err(_) => "https://pc-stats.eov2.com".to_string(),
+    };
 
     println!("Hello, world!");
 
     let mut sys = System::new_all();
-    sys.refresh_all();
-
-    let cpu_name = sys.cpus()[0].brand().to_string();
-    let os_name = sys.name().expect("Failed to get os name");
-    let os_version = sys.os_version().expect("Failed to get os version");
-    let hostname = sys.host_name().expect("Failed to get hostname");
-
-    println!("System OS: {} {}", os_name, os_version);
-
-    let mut system_info = StatusData {
-        _os: format!("{} {}", os_name, os_version),
-        hostname: hostname,
-        version: "".to_string(),
-        cpu: CpuData {
-            model: "".to_string(),
-            cpus: vec![],
-            percent: 0,
-        },
-    };
-
-    println!("{}", json!(system_info));
 
     let send_system_info = |payload: Payload, socket: RawClient| match payload {
         Payload::String(str) => {
@@ -49,8 +34,8 @@ fn main() {
         _ => {}
     };
 
-    let socket = ClientBuilder::new("http://localhost:4200")
-        .namespace("/")
+    let socket = ClientBuilder::new(PCSC_URI)
+        .namespace("/server")
         .on("connect", |_, _| println!("Connected"))
         .on("disconnect", |_, _| println!("Disconnected"))
         .on("hi", send_system_info)
@@ -59,50 +44,54 @@ fn main() {
         .connect()
         .expect("Connection failed");
 
-    let json_payload = json!({"token": 123});
+    /*     let json_payload = json!({"token": 123});
     socket
         .emit("foo", json_payload)
         .expect("Server unreachable");
+     */
 
-    // Update all information
-
-    let load_avg = sys.load_average();
-    println!(
-        "one minute: {}, five minutes: {}, fifteen minutes: {}",
-        load_avg.one, load_avg.five, load_avg.fifteen,
-    );
-
-    // Disks
-    println!("=> disks:");
-    for disk in sys.disks() {
-        println!("{:?}", disk);
-    }
-
-    println!("=> system:");
-    // RAM and swap:
-    println!("total memory: {} bytes", sys.total_memory());
-    println!("used memory : {} bytes", sys.used_memory());
-    println!("total swap  : {} bytes", sys.total_swap());
-    println!("used swap   : {} bytes", sys.used_swap());
-
-    // System information:
-    println!(
-        "System host name: {}",
-        sys.host_name().expect("Failed to get hostname")
-    );
-
-    // Number of CPUs:
-    println!("NB CPUs: {}", sys.cpus().len());
-
-    // CPU Usage
     loop {
-        sys.refresh_cpu(); // Refreshing CPU information.
-        println!("---------------");
-        //println!("{}", sys.cpus());
-        for cpu in sys.cpus() {
-            println!("{}%", cpu.cpu_usage());
+        sys.refresh_all();
+
+        let cpu_name = sys.cpus()[0].brand().to_string();
+        let os_name = sys.name().expect("Failed to get os name");
+        let os_version = sys.os_version().expect("Failed to get os version");
+        let hostname = sys.host_name().expect("Failed to get hostname");
+
+        let load_avg = sys.load_average();
+        let loadavg: Option<[f64; 3]> = match os_name.as_str() {
+            "Windows" => None,
+            _ => Some([load_avg.one, load_avg.five, load_avg.fifteen]),
+        };
+
+        println!("System OS: {} {}", os_name, os_version);
+
+        let mut system_info = StatusData {
+            _os: format!("{} {}", os_name, os_version),
+            hostname: hostname,
+            version: "rust".to_string(),
+            cpu: CpuData {
+                model: cpu_name.to_string(),
+                cpus: vec![],
+                percent: 0,
+            },
+            loadavg,
+        };
+
+        println!("=> disks:");
+        for disk in sys.disks() {
+            println!("{:?}", disk);
         }
-        println!("---------------");
+
+        println!("=> system:");
+        println!("total memory: {} bytes", sys.total_memory());
+        println!("used memory : {} bytes", sys.used_memory());
+
+        println!("{}", json!(system_info));
+
+        for cpu in sys.cpus() {
+            //println!("{}%", cpu.cpu_usage());
+        }
         std::thread::sleep(Duration::from_secs(1));
     }
 }
--- src/status/mod.rs
+++ src/status/mod.rs
@@ -1,21 +1,22 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct CpuCoreUsage {
-  pub(crate) cpu: i16
+    pub(crate) cpu: i16,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct CpuData {
-  pub(crate) model: String,
-  pub(crate) cpus: Vec<CpuCoreUsage>,
-  pub(crate) percent: i16
+    pub(crate) model: String,
+    pub(crate) cpus: Vec<CpuCoreUsage>,
+    pub(crate) percent: i16,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct StatusData {
-  pub(crate) _os: String,
-  pub(crate) hostname: String,
-  pub(crate) version: String,
-  pub(crate) cpu: CpuData
+    pub(crate) _os: String,
+    pub(crate) hostname: String,
+    pub(crate) version: String,
+    pub(crate) cpu: CpuData,
+    pub(crate) loadavg: Option<[f64; 3]>,
 }
