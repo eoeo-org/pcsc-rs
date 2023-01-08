4fb1e0e0a8dada7e9b9bd47a534104a9a343179e
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-01-08 16:22:39 +0900

    Show minimal information (test) (#5)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,76 @@
+mod status;
+
 use dotenvy::dotenv;
+use rust_socketio::{ClientBuilder, Payload, RawClient};
+use serde_json::json;
 use std::env;
+use std::time::Duration;
 use sysinfo::{CpuExt, System, SystemExt};
+
+use crate::status::{CpuData, CpuCoreUsage, StatusData};
 
 fn main() {
     dotenv().expect(".env file not found");
 
+    /*for (key, value) in env::vars() {
+        println!("{key}: {value}");
+    }*/
+
     println!("Hello, world!");
 
-    for (key, value) in env::vars() {
-        println!("{key}: {value}");
-    }
-
     let mut sys = System::new_all();
+    sys.refresh_all();
+
+    let cpu_name = sys.cpus()[0].brand().to_string();
+    let os_name = sys.name().expect("Failed to get os name");
+    let os_version = sys.os_version().expect("Failed to get os version");
+    let hostname = sys.host_name().expect("Failed to get hostname");
+
+    println!("System OS: {} {}", os_name, os_version);
+
+    let mut system_info = StatusData {
+        _os: format!("{} {}", os_name, os_version),
+        hostname: hostname,
+        version: "".to_string(),
+        cpu: CpuData {
+            model: "".to_string(),
+            cpus: vec![],
+            percent: 0,
+        },
+    };
+
+    println!("{}", json!(system_info));
+
+    let send_system_info = |payload: Payload, socket: RawClient| match payload {
+        Payload::String(str) => {
+            println!("Received: {}", str);
+            //socket.emit("test", json!(system_info)).expect("Failed to emit.");
+        }
+        _ => {}
+    };
+
+    let socket = ClientBuilder::new("http://localhost:4200")
+        .namespace("/")
+        .on("connect", |_, _| println!("Connected"))
+        .on("disconnect", |_, _| println!("Disconnected"))
+        .on("hi", send_system_info)
+        .on("sync", send_system_info)
+        .on("error", |err, _| eprintln!("Error: {:#?}", err))
+        .connect()
+        .expect("Connection failed");
+
+    let json_payload = json!({"token": 123});
+    socket
+        .emit("foo", json_payload)
+        .expect("Server unreachable");
 
     // Update all information
-    sys.refresh_all();
+
+    let load_avg = sys.load_average();
+    println!(
+        "one minute: {}, five minutes: {}, fifteen minutes: {}",
+        load_avg.one, load_avg.five, load_avg.fifteen,
+    );
 
     // Disks
     println!("=> disks:");
@@ -30,11 +86,10 @@ fn main() {
     println!("used swap   : {} bytes", sys.used_swap());
 
     // System information:
-    let os_name = sys.name().expect("Failed to get os name");
-    let os_version = sys.os_version().expect("Failed to get os version");
-    
-    println!("System OS: {} {}", os_name, os_version);
-    println!("System host name: {}", sys.host_name().expect("Failed to get hostname"));
+    println!(
+        "System host name: {}",
+        sys.host_name().expect("Failed to get hostname")
+    );
 
     // Number of CPUs:
     println!("NB CPUs: {}", sys.cpus().len());
@@ -48,6 +103,6 @@ fn main() {
             println!("{}%", cpu.cpu_usage());
         }
         println!("---------------");
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        std::thread::sleep(Duration::from_secs(1));
     }
 }
